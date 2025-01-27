184e675b5a0b4c3e9f9895ca6855e783556d8106
Chad Wilson <20445056+NetPenguins@users.noreply.github.com>
2025-01-27 03:24:29 +0000

    Add Projects page and update navigation links; replace example project with XSS Mania

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'XSS Mania',
+    description: `Demonstrating the power of XSS through basic DOM Baseed XSS examples. This is a static page with no backend, or router capabilities therefore Reflected XSS and Stored XSS will not work here.`,
+    imgSrc: '/static/images/xssmania.jpeg',
+    href: '/static/pages/xssmania.html',
   },
 ]
 
